3c2e2a1bc88a2f6c69952067178b3b79548a72ef
Maxime Lalisse <maxime.lalisse@gmail.com>
2019-10-25 15:48:44 +0200

    Add inbox component (mostly empty for now)

---
+++ src/components/Inbox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="inbox">
+    <p>This is inbox</p>
+    <div>
+      <div
+        v-for="msg in messages"
+        v-bind:key="msg.id">
+        {{msg}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Inbox',
+  props: {
+    messages: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
