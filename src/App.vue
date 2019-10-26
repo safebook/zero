89b916f46a95cecdd7ff847c60309f7e337f811e
Maxime Lalisse <maxime.lalisse@gmail.com>
2019-10-26 18:38:51 +0200

    Add tabs for messages, contacts and profile (pubkey)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Inbox
-      :messages="messages"
-      @submitMessage="submitMessage" />
-    <!-- can maybe see inbox even if unlogged -->
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div id="header">
+      <div>
+        <a href
+           @click.prevent="tab = 'messages'"
+           v-bind:class="{ active: tab == 'messages' }"
+           >Messages</a>
+      </div>
+      <div>
+        <a href
+           @click.prevent="tab = 'contacts'"
+           v-bind:class="{ active: tab == 'contacts' }"
+           >Contacts</a>
+      </div>
+      <div>
+        <a href
+           @click.prevent="tab = 'profile'"
+           v-bind:class="{ active: tab == 'profile' }"
+           >Profile</a>
+      </div>
+    </div>
     <Login v-if="!logged" @login="login" />
+    <p class="pubkey" v-if="tab == 'profile'">
+      {{encodedPubkey()}}
+    </p>
+    <Inbox v-if="tab == 'messages'"
+        :messages="messages"
+        @submitMessage="submitMessage" />
+    <Contacts
+      v-if="tab == 'contacts'"
+      :contacts="contacts"
+      @addContact="addContact" />
   </div>
 </template>
 
@@ -13,6 +39,8 @@
 /* global sjcl */
 import Login from './components/Login.vue'
 import Inbox from './components/Inbox.vue'
+import Contacts from './components/Contacts.vue'
+import base58 from 'bs58'
 
 export default {
   name: 'app',
@@ -22,7 +50,8 @@ export default {
       pubkey: null,
       seckey: null,
       contacts: [],
-      messages: []
+      messages: [],
+      tab: 'messages'
     }
   },
   methods: {
@@ -40,6 +69,20 @@ export default {
         id = this.messages[this.messages.length - 1].id + 1
       }
       this.messages.push({ id: id, data: data })
+    },
+    encodedPubkey () {
+      if (this.pubkey) {
+        return base58.encode(Buffer.from(sjcl.codec.bytes.fromBits(this.pubkey)))
+      } else {
+        return '(null)'
+      }
+    },
+    addContact (alias, pubkey) {
+      console.log(alias, pubkey)
+      this.contacts.push({
+        alias: alias,
+        pubkey: pubkey
+      })
     }
   },
   created () {
@@ -49,7 +92,8 @@ export default {
   },
   components: {
     Login,
-    Inbox
+    Inbox,
+    Contacts
   }
 }
 </script>
@@ -62,5 +106,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.pubkey {
+  margin-left: 33%;
+  width: 33%;
+  word-break: break-all;
+}
+#header {
+  display: grid;
+  grid-template-columns: 30% 30% 30%;
+  margin-left: 10%;
 }
 </style>
