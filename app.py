9a865e9335060ba90f354794892f4f07b55e2df2
Maxime Lalisse <maxime.lalisse@gmail.com>
2019-11-04 17:35:08 +0100

    Add server

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, escape, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, static_url_path='')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.Text)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'data': self.data
+        }
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/messages')
+def messages():
+    messages = Message.query.all()
+    return jsonify(messages = [m.serialize() for m in messages])
